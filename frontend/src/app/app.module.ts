81aa3deea0a41c82b9effeebeecbeae232e829ff
Balázs Kis <19716399+balazs-kis@users.noreply.github.com>
2018-08-25 10:36:22 +0200

    Adding angular material for styling

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,7 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
+import { MaterialModule } from './shared/material.module';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,6 +14,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
