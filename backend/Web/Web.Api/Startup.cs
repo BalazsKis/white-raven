206dab7da77d7c04ddce38f719266ba8158d06f5
Balázs Kis <19716399+balazs-kis@users.noreply.github.com>
2018-08-26 21:04:50 +0200

    CORS: round 3

--- backend/Web/Web.Api/Startup.cs
+++ backend/Web/Web.Api/Startup.cs
@@ -73,7 +73,7 @@ namespace WhiteRaven.Web.Api
             app.UseAuthentication();
 
             // Allow cross-domain requests
-            app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod());
+            app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader().AllowCredentials());
 
             // Common exception catching for all controllers
             app.UseMiddleware<ErrorHandlingMiddleware>();
